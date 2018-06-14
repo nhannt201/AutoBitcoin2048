d03da4205b5b9fb5aa76b73f925dcca31184f04d
me09112001 <trungnhan21.12@gmail.com>
2018-06-14 09:08:34 +0700

    Code Auto 2047
    Code Auto 2047

---
+++ Bitcoin2048 Auto/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 42)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Tự chơi 2048 (F1)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(184, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 42)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Dừng tự chơi (F2)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 80000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Thời gian :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(75, 60)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(243, 20)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "150"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(113, 89)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox2.TabIndex = 6
+        Me.TextBox2.Text = "max"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Thời gian tải trang:"
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1200
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 300
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 121)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto 2048 BTC by Nguyễn Trung Nhẫn"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+End Class
---
+++ Bitcoin2048 Auto/Form1.vb
@@ -0,0 +1,102 @@
+﻿Public Class Form1
+
+    Dim a As String = "1"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' wb.Navigate("http://bitcoin2048.com/")
+        '  System.Windows.Forms.SendKeys.SendWait("{ENTER}")
+        '  SendKeys.Send("{ENTER}")
+        '  SendKeys.Send("{LEFT}")
+        '  SendKeys.Send("{ENTER}")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            If a = "1" Then
+                SendKeys.Send("{UP}")
+                a = a + 1
+            ElseIf a = "2" Then
+                SendKeys.Send("{LEFT}")
+                a = a + 1
+            ElseIf a = "3" Then
+                SendKeys.Send("{DOWN}")
+                a = a + 1
+            ElseIf a = "4" Then
+                SendKeys.Send("{RIGHT}")
+                a = "1"
+            End If
+            '  Label2.Text = Label2.Text + 1
+        Catch
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Interval = TextBox1.Text
+
+        Timer1.Start()
+        Timer3.Start()
+        Timer4.Start()
+        If TextBox2.Text = "max" Then
+            Timer2.Stop()
+
+        Else
+            Timer2.Start()
+            Timer2.Interval = TextBox2.Text
+        End If
+        Button2.Enabled = True
+
+        Button1.Enabled = False
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        'wb.Document.GetElementById("retry-button").InvokeMember("Click")
+        '   wb.Document.GetElementById("keep-playing-button").InvokeMember("Click")
+        '  wb.Document.GetElementById("continue-button").InvokeMember("Click")
+        SendKeys.Send("{ENTER}")
+        SendKeys.Send("{F5}")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+        Timer2.Stop()
+        Timer3.Stop()
+        Timer4.Stop()
+        Button1.Enabled = True
+        Button2.Enabled = False
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.F1 Then
+            Timer1.Start()
+            Timer2.Start()
+
+        End If
+        If e.KeyCode = Keys.F2 Then
+            Timer1.Stop()
+            Timer2.Stop()
+
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Timer1.Interval = "300"
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        Timer1.Interval = TextBox1.Text
+    End Sub
+End Class
